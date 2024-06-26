343c4c08e21e2f9f17f8ab65de95c4ebc10aa206
Dinesh Jumani <jumanidieneshdj@gmail.com>
2024-06-26 17:35:15 +0530

    Auth Form Added Login and Sign up form

---
+++ 15AuthForms/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginSignup from './components/login_signup/LoginSignup';
+
+
+function App() {
+    return (
+        <div>
+            <LoginSignup />
+        </div>
+    )
+}
+
+export default App
